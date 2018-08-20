0b1edbdccfb0d9f48d101eca660d97220a36fd6a
Felix <fleix@gmx.ch>
2018-08-20 09:41:37 +0200

    Deactivate Kickstarter

---
+++ site/accounts/felix.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: felix
+password: >
+  $2a$10$J8Lb6wgjKWwuKJPZr8e5TOYGP5mlgLsjulCCf0Cu87HDYgIOEcTCS
+email: fleix@gmx.ch
+language: de
+role: admin
--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -1,23 +1,47 @@
 		<!-- NAVBAR -->
 
-		<!-- KICKSTARTER FLAG -->
+		<!-- KICKSTARTER FLAG
 		<a href="https://www.kickstarter.com/projects/582360255/swiss-to-go?lang=de">
 			<div class="k">
 				<img src="./content/k_logo.svg">
 			</div>
-		</a>
+		</a> -->
 
 		<div class="container">
 			<div class="row">
-				<div class="navbar col-xl-12 col-lg-12 col-md-12 col-sm-12 d-flex justify-content-between flex-nowrap">
-					<div class="col-lg-2 col-xs-7 m-0">
-					<a href="<?= url('swisstogo') ?>" class="brand" >
-						<img class="brand" src="<?= $site->logo()->toFile()->url() ?>">
-					</a>
+				<div class="navbar col-xl-12 col-lg-12 col-md-12 col-sm-12 d-flex justify-content-end flex-nowrap">
+					<div class="col-lg-8 col-xs-7 flex-grow-1 m-0">
+						<a href="<?= url('swisstogo') ?>" class="brand" >
+							<img class="brand" src="<?= $site->logo()->toFile()->url() ?>">
+						</a>
 					</div>
-					<div class="col-lg-1 col-xs-3 offset-3 p-3">
-						<h4><a target="_blank" href="http://swisstoolcase.com/product/swiss-to-go/">Buy</a></h4>
+
+					<!-- LANGUAGE-SWITCHER
+					<div class="switch col-lg-2">
+						<nav class="languages" role="navigation">
+							<ul>
+								<?php foreach($site->languages() as $language): ?>
+								<li<?php e($site->language() == $language, ' class="active"') ?>>
+									<a href="<?= $page->url($language->code()) ?>">
+										<?= html($language->name()) ?>
+									</a>
+								</li>
+								<?php endforeach ?>
+							</ul>
+						</nav>
+					</div> -->
+
+					<!-- BUY -->
+					<div class="col-lg-1 col-xs-3">
+						<button class="btn btn-hero">
+						<a target="_blank" href="http://swisstoolcase.com/product/swiss-to-go/">Buy</a>
+					</button>
 					</div>
+
+
+
+
+
 				</div>  <!-- END NAV -->
 			</div>
 		</div>
