5c0dfc1804b438dd2eca2d02d1b78086a7a67114
Felix <fleix@gmx.ch>
2018-08-20 09:59:38 +0200

    Navbar Buy Button position mobile

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -32,7 +32,7 @@
 					</div> -->
 
 					<!-- BUY -->
-					<div class="col-lg-1 col-xs-3">
+					<div class="col-lg-1 col-md-1 col-sm-1">
 						<button class="btn btn-hero">
 						<a target="_blank" href="http://swisstoolcase.com/product/swiss-to-go/">Buy</a>
 					</button>
